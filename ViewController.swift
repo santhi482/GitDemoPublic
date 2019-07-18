df48d15cd1cdb1bafa9e1a025b2ddc369a9bbd50
santhi482 <53034635+santhi482@users.noreply.github.com>
2019-07-18 12:22:47 +0530

    added a VC

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoGitHub
+//
+//  Created by Santhi Priya  on 18/07/19.
+//  Copyright © 2019 Santhi Priya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world")
+    }
+
+
+}
+
